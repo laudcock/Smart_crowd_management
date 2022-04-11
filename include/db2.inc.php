c32872ba37b23f32203dc58847829db223670e09
laudcock <59482795+laudcock@users.noreply.github.com>
2022-04-11 16:26:53 +0200

    Delete db2.inc.php

--- include/db2.inc.php
+++
@@ -1,4 +0,0 @@
-<?php
-$conn_string = "host=localhost port=5432 dbname=crowd_routing_belgium user=crowd password=TD2XAPeWuDJUG9dW";
-$GLOBALS['dbconn'] = pg_connect($conn_string) or die("Could not connect");
-?>
