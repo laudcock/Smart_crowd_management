3963be573a8459c6c670e464d7016151cff39351
laudcock <59482795+laudcock@users.noreply.github.com>
2022-04-11 16:26:08 +0200

    Update db.inc.php

--- include/db.inc.php
+++ include/db.inc.php
@@ -1,4 +1,4 @@
 <?php
-$conn_string = "host=localhost port=5432 dbname=crowd user=crowd password=TD2XAPeWuDJUG9dW";
+$conn_string = "host=localhost port=5432 dbname=crowd user=crowd password=anonymized";
 $GLOBALS['dbconn'] = pg_connect($conn_string) or die("Could not connect");
 ?>
